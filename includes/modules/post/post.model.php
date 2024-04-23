17807486ce887232aae0e9caf83b45491666a010
singleview-co-kr <w9721066@gmail.com>
2024-04-23 12:16:26 +0900

    Fix minor error
    modified:   includes/modules/post/post.model.php

--- includes/modules/post/post.model.php
+++ includes/modules/post/post.model.php
@@ -990,7 +990,6 @@ var_dump('plz define category search');
 					$cache_key = $o_cache_handler->getGroupKey('site_and_module', $object_key);
 					$a_keys = $o_cache_handler->get($cache_key);
 				}
-				unset($o_cache_handler);
 				// $o_user_define_fields = \X2board\Includes\Classes\UserDefineFields::getInstance($n_board_id); // 호출 효율성 위해서 아래로 이동
 
 				if($a_keys === false) {  // _set_user_define_fields()과 동일한 DB 호출  -> 캐쉬화해야 함
@@ -1050,6 +1049,7 @@ var_dump('plz define category search');
 						$o_cache_handler->put($cache_key, $a_keys);
 					}
 				}
+				unset($o_cache_handler);
 				$G_X2B_CACHE['X2B_USER_DEFINE_KEYS'][$n_board_id] = $a_keys;
 			}
 			return $G_X2B_CACHE['X2B_USER_DEFINE_KEYS'][$n_board_id];
